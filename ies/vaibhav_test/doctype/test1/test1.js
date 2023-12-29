d53aa72c134bf80e3c8913a608bbc4809addd1f0
vaibhav <vaibhav.shukla@affle.com>
2023-12-29 15:30:57 +0530

    test1 Doctype and Vaibhav_test Module added

---
+++ ies/vaibhav_test/doctype/test1/test1.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Affle and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Test1", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ ies/vaibhav_test/doctype/test1/test1.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Affle and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Test1(Document):
+	pass
